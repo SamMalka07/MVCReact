203f2091fe92d756f1b0523c4f3817ab80f6e9b2
Sam Malka <109494070+SamMalka07@users.noreply.github.com>
2023-01-18 01:39:25 +1300

    Update CustomerController.cs

--- MvcReact/Controllers/CustomerController.cs
+++ MvcReact/Controllers/CustomerController.cs
@@ -30,68 +30,6 @@ namespace MvcReact.Controllers
             return Json(editingCustomer);
         }
 
-        [HttpPost]
-        public IActionResult Create([FromBody] Customer data)
-        {
-
-            ModelState.Remove("Sales");
-
-            if (ModelState.IsValid)
-            {
-                Customer customerObj = new()
-                {
-                    Name = data.Name,
-                    Address = data.Address,
-                };
-                _db.Customers.Add(customerObj);
-                _db.SaveChanges();
-                return Json(new { status = "success" });
-            }
-            else
-            {
-                var errors = ModelState.Select(x => x.Value.Errors)
-                           .Where(y => y.Count > 0)
-                           .ToList();
-
-                return Json(new { status = "error", message=errors});
-            }
-            
-        }
-
-        [HttpPost]
-        public IActionResult Edit([FromBody] Customer data)
-        {
-
-            ModelState.Remove("Sales");
-
-            if (ModelState.IsValid)
-            {
-                _db.Customers.Update(data);
-                _db.SaveChanges();
-                return Json(new { status = "success" });
-            }
-            else
-            {
-                var errors = ModelState.Select(x => x.Value.Errors)
-                           .Where(y => y.Count > 0)
-                           .ToList();
-
-                return Json(new { status = "error", message = errors });
-            }
-
-        }
-
-        [HttpDelete]
-        public IActionResult Delete(int id)
-        {
-            var findCustomer = _db.Customers.Find(id);
-            if(findCustomer != null)
-            {
-                _db.Customers.Remove(findCustomer);
-                _db.SaveChanges();
-                return Json(new { status = "success" });
-            }
-            return Json(new {status="error"});
-        }
+        
     }
 }
